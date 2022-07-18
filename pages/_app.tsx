5abab5e6359665cf6ef011d908d89180f966920b
Mac-DevOSYandex <MacDev-Admin@protonmail.com>
2022-07-18 15:25:19 +0100

    feat: add header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { AppProps } from 'next/app';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode, useEffect } from 'react';
+// import 'tw-elements/dist/css/index.min.css';
+// import 'tw-elements';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function CustomApp({ Component, pageProps }: AppPropsWithLayout) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default CustomApp;
+
+// useEffect(() => {
+//   import('tw-elements');
+//   // import ('tw-elements/dist/css/index.min.css)';
+// }, []);
